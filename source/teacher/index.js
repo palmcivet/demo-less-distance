1c80df79659b46ef8248bf66d0fe03922664482c
Palm Civet <palmcivet_email@qq.com>
2020-04-10 20:46:22 +0800

    :star2: 增加标注工具

--- source/teacher/index.js
+++ source/teacher/index.js
@@ -1,3 +1,15 @@
+const typeStyle = [
+	"arrow",
+	"line",
+	"rectangle",
+	"triangle",
+	"circle",
+	"text",
+	// "pen",
+	// "eraser",
+	// "undo",
+	// "redo",
+];
 const user = {};
 
 const config = {
@@ -7,6 +19,7 @@ const config = {
 	canvasCtx: null, // canvas 的 context
 	paintNode: null, // paint 节点，通过 ID 获取
 	paintCtx: null, // paint 的 context
+	proxyNode: null,
 };
 
 const store = {
@@ -14,17 +27,53 @@ const store = {
 	pdfPageNum: 0, // PDF 页数
 	currentPage: 0, // 当前页
 	currentScale: 1, // 缩放比例
+	// 绘图
+	drawingSurface: null, // 保存绘图表面
+	mouseDown: null, // 保存鼠标按下时的 canvas 坐标
+	dragging: false, // 标识鼠标是否处于拖拽状态
+	color: "red",
+	size: 14,
+	type: 0,
 };
 
 $(document).ready(() => {
-	if (null === (config.canvasNode = document.getElementById("canvas-node"))) {
+	if (null === (config.canvasNode = $("#canvas-node")[0])) {
 		alert("您的浏览器不支持 Canvas");
 	} else {
+		config.proxyNode = $("#proxy")[0];
 		config.canvasCtx = config.canvasNode.getContext("2d");
-		config.paintNode = document.getElementById("canvas-paint");
+		config.paintNode = $("#canvas-paint")[0];
 		config.paintCtx = config.paintNode.getContext("2d");
 		config.paintNode.fillStyle = "rgba(255, 255, 255, 0)";
 		pdfjsLib.workerSrc = config.libSrc;
+		// 生成标注工具
+
+		let ul = typeStyle.map((i, j) => `<li>${genIcon(j)}</li>`);
+		$("ul").append(ul);
+		for (let item = 0; item < typeStyle.length; item++) {
+			$("ul li")[item].onclick = () => changeType(item);
+			if (item === 0) {
+				changeType(0, true);
+			}
+		}
+
+		config.proxyNode.addEventListener("compositionstart", (e) => {
+			e.target.inputStatus = "CHINESE_TYPING";
+			console.log("start");
+		});
+		config.proxyNode.addEventListener("input", (e) => {
+			if (e.target.inputStatus !== "CHINESE_TYPING") {
+				console.log(e.target.inputStatus);
+				toolBox[typeStyle[5]](store.mouseDown, e.target.value);
+			}
+		});
+		config.proxyNode.addEventListener("compositionend", (e) => {
+			setTimeout(() => {
+				console.log("end");
+				e.target.inputStatus = "CHINESE_TYPE_END";
+				toolBox[typeStyle[5]](store.mouseDown, e.target.value);
+			}, 100);
+		});
 	}
 });
 
@@ -36,6 +85,29 @@ $("#upload")[0].addEventListener("change", () => {
 			(pdf) => {
 				store.pdfContent = pdf;
 				gotoPage((store.currentPage = 1));
+				// 设置监听器
+				config.paintNode.addEventListener("mousedown", (event) => {
+					if (store.type === 5) {
+						config.proxyNode.value = null;
+					}
+					store.mouseDown = windowToCanvas(event.clientX, event.clientY);
+					store.dragging = true;
+					saveDrawingSurface();
+				});
+				config.paintNode.addEventListener("mousemove", (event) => {
+					if (store.dragging && store.type !== 0 && store.type !== 5) {
+						restoreDrawingSurface();
+						let mouseMove = windowToCanvas(event.clientX, event.clientY);
+						let toolId = typeStyle[store.type];
+						toolBox[toolId](mouseMove);
+					}
+				});
+				config.paintNode.addEventListener("mouseup", (event) => {
+					store.dragging = false;
+					if (store.type === 5) {
+						config.proxyNode.focus();
+					}
+				});
 			},
 			() => {
 				// TODO: 优化提示
--- source/teacher/utils.js
+++ source/teacher/utils.js
@@ -2,7 +2,6 @@ const gotoPage = (page, scale = 1) => {
 	store.pdfContent.getPage(page).then(
 		(resPage) => {
 			let viewport = resPage.getViewport({ scale });
-			// ALTER
 			let canvas = config.canvasNode;
 			let context = config.canvasCtx;
 			canvas.height = viewport.height;
@@ -12,7 +11,6 @@ const gotoPage = (page, scale = 1) => {
 				viewport: viewport,
 			};
 			resPage.render(renderContext);
-			// ALTER
 			store.currentScale = viewport.scale;
 			config.paintNode.width = viewport.width;
 			config.paintNode.height = viewport.height;
@@ -36,4 +34,108 @@ const zoomOut = () => {
 
 const zoomIn = () => {
 	gotoPage(store.currentPage, store.currentScale + 0.1);
+};
+
+// 将浏览器客户区坐标转换为 canvas 坐标
+function windowToCanvas(clientX, clientY) {
+	let axisCanvas = config.paintNode.getBoundingClientRect();
+	return {
+		x: clientX - axisCanvas.left,
+		y: clientY - axisCanvas.top,
+	};
+}
+
+// 保存 canvas 绘图表面
+function saveDrawingSurface() {
+	store.drawingSurface = config.paintCtx.getImageData(
+		0,
+		0,
+		config.paintNode.width,
+		config.paintNode.height
+	);
+}
+
+// 恢复 canvas 绘图表面
+function restoreDrawingSurface() {
+	config.paintCtx.putImageData(store.drawingSurface, 0, 0);
+}
+
+// 生成图标
+const genIcon = (classId, isSelected = false) => {
+	return isSelected
+		? "<i class='" + typeStyle[classId] + " selected" + "'></i>"
+		: "<i class='" + typeStyle[classId] + "'></i>";
+};
+
+// 修改线型
+const changeType = (typeId) => {
+	$("ul li")[store.type].innerHTML = genIcon(store.type);
+	store.type = typeId;
+	$("ul li")[typeId].innerHTML = genIcon(typeId, true);
+};
+
+// 修改粗细
+
+// 修改颜色
+
+const toolBox = {
+	//  ["arrow", "line", "rectangle", "triangle", "circle", "text", "pen",	"eraser", "undo", "redo"]
+
+	line: (mouseMove) => {
+		config.paintCtx.beginPath(); // 清除当前路径
+		config.paintCtx.moveTo(store.mouseDown.x, store.mouseDown.y);
+		config.paintCtx.lineTo(mouseMove.x, mouseMove.y);
+		config.paintCtx.strokeStyle = store.color;
+		config.paintCtx.stroke();
+	},
+	rectangle: (mouseMove) => toolBox.polygon(mouseMove, 4),
+	triangle: (mouseMove) => toolBox.polygon(mouseMove, 3),
+	circle: (mouseMove) => {
+		config.paintCtx.beginPath();
+		// 使用勾股定理圆的计算半径
+		let r = Math.sqrt(
+			Math.pow(mouseMove.x - store.mouseDown.x, 2) +
+				Math.pow(mouseMove.y - store.mouseDown.y, 2)
+		);
+		config.paintCtx.arc(
+			store.mouseDown.x,
+			store.mouseDown.y,
+			r,
+			0,
+			2 * Math.PI,
+			true
+		);
+		config.paintCtx.strokeStyle = store.color;
+		config.paintCtx.stroke();
+	},
+	text: (mouseMove, text) => {
+		config.paintCtx.font = "16px 'SFPing Fang'";
+		config.paintCtx.fillStyle = store.color;
+		config.paintCtx.fillText(text, mouseMove.x, mouseMove.y);
+	},
+	pen: (mouseMove) => {},
+	// 绘制多边形
+	polygon: (mouseMove, sides) => {
+		config.paintCtx.beginPath(); // 清除当前路径
+		// 多边形外接圆半径
+
+		let r = Math.sqrt(
+			Math.pow(store.mouseDown.x - mouseMove.x, 2) +
+				Math.pow(store.mouseDown.y - mouseMove.y, 2)
+		);
+		let angle = 0;
+		for (let i = 0; i < sides; i++) {
+			let vertex_x = store.mouseDown.x + r * Math.sin(angle);
+			let vertex_y = store.mouseDown.y - r * Math.cos(angle);
+			if (i == 0) {
+				config.paintCtx.moveTo(vertex_x, vertex_y); // 多边形起始顶点
+			} else {
+				config.paintCtx.lineTo(vertex_x, vertex_y); // 多边形其余顶点
+			}
+			angle += (2 * Math.PI) / sides;
+		}
+		config.paintCtx.closePath();
+		config.paintCtx.strokeStyle = store.color;
+		config.paintCtx.stroke();
+	},
 };
